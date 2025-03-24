433d5bbbdcdebae277ea6e8f9cc077aa2668b4a8
senan25252 <senan.yusubov45@gmail.com>
2025-03-24 11:26:11 +0400

    UPLOAD

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿const button = document.getElementById("generatePassword");
+const label = document.getElementById("password");
+
+const passwordRandomizer = "qwertyuiopasdfghjklzxcvbnm!@#$%^"
+
+button.onclick = function() {
+    let index = Math.random() * 31;
+    let password = "";
+
+    while(password.length < 12) {
+        password += passwordRandomizer.charAt(index);
+        index = Math.random() * 31;
+    }
+
+    label.textContent = password;
+
+}
